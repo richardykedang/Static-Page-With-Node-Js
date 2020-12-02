973cfd899b51a31d0df69d0a3e655259a6f3e606
richardykedang <kedangrichard@gmail.com>
2020-12-02 14:20:49 +0700

    import replaceTemp Module to entry point

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const replaceTemplate = require('./modules/replaceTemplate');
 const { type } = require('os');
 // // blocking synchronus
 const textIn = fs.readFileSync('./text/input.txt','utf-8');
@@ -23,20 +24,6 @@ fs.readFile('./text/asyncOne.txt','utf-8',(err,data) => {
 console.log('File Was Be Written');
 
 // SERVER
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-    //output = output.replace(/{%ORGANIC%}/g, product.organic);
-    if(!product.organic)output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    return output;
-}
-
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-cards.html`, 'utf-8');
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-products.html`, 'utf-8');
